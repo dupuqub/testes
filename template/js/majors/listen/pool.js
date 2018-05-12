c3cbecbdd05710761d5c17779ab6142ba8ace1d0
= <dupuqub@gmail.com>
2018-05-12 16:48:25 -0300

    fix template code style + code listening example

--- template/js/majors/listen/pool.js
+++ template/js/majors/listen/pool.js
@@ -1,12 +1,19 @@
 
-'use strict'
+`use strict`
 
 //......................................................................................................................
 
 E.listen.pool =
 [
   {
-    id : 'view' ,
+    id : `template` ,
+    active : true ,
+    kill : true ,
+    test : () => true ,
+    act : () => console.log(`code listening template runned`) ,
+  } ,
+  {
+    id : `view` ,
     active : true ,
     kill : false ,
     test : () => E.info.view !== E.state.view ,
